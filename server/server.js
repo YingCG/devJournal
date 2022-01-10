66b906a48249a36f6970f066814d6bdcac3b8f30
YingCG <ying@DESKTOP-VQDFUEH.localdomain>
2022-01-10 15:07:16 +1300

    try deploy

--- server/server.js
+++ server/server.js
@@ -6,8 +6,10 @@ const routes = require('./routes/routes')
 
 const server = express()
 server.use(express.json())
-server.use(express.static(path.join(__dirname, 'public')))
-
+// server.use(express.static(path.join(__dirname, 'public')))
+// app.get('/*', function (req, res) {
+//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//  });
 server.use('/api/v1/todos', routes)
 
 module.exports = server
