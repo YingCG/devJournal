3dab4197fe8d57455bf84d6bc338352641d431da
YingCG <yingchiugoh@gmail.com>
2022-01-10 22:01:56 +1300

    Procfile

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const express = require('express')
-const routes = require('./routes/routes')
-
-
 const server = express()
+
+module.exports = server
+
+const routes = require('./routes/routes')
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/todos', routes)
 
-module.exports = server
